0db1c1c3f0c1e860086f1fa27e2981271cbf69f2
Hartono Halim <ah3555@Halff.com>
2019-02-01 11:38:52 -0600

    first commit

--- DotNetZipTest/Program.cs
+++ DotNetZipTest/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Ionic.Zip;
+using System;
 
 namespace DotNetZipTest
 {
     class Program
     {
+
+        private static void Usage()
+        {
+            Console.WriteLine("usage:\n  ZipDir <ZipFileToCreate> <directory>");
+            Environment.Exit(1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 2) Usage();
+            if (!System.IO.Directory.Exists(args[1]))
+            {
+                Console.WriteLine("The directory does not exist!\n");
+                Usage();
+            }
+            if (System.IO.File.Exists(args[0]))
+            {
+                Console.WriteLine("That zipfile already exists!\n");
+                Usage();
+            }
+            if (!args[0].EndsWith(".zip"))
+            {
+                Console.WriteLine("The filename must end with .zip!\n");
+                Usage();
+            }
+
+            string TargetZipFile = args[0];
+            string SourceDirectory = args[1];
+            try
+            {
+                var progress = new ZipProgress(5);
+                using (ZipFile zip = new ZipFile())
+                {
+                    zip.AddProgress += progress.AddProgress;
+                    zip.StatusMessageTextWriter = Console.Out;
+                    zip.AddDirectory(SourceDirectory); // recurses subdirectories
+
+                    var count = zip.Count; //number of files and sub folders added
+
+                    //zip.BufferSize = 4096;
+                    //zip.SortEntriesBeforeSaving = true;
+                    zip.SaveProgress += progress.SaveProgress;
+                    zip.Save(TargetZipFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("exception: " + ex);
+            }
         }
+
+
+
     }
 }
---
+++ DotNetZipTest/ZipProgress.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zip;
+
+namespace DotNetZipTest
+{
+    public class ZipProgress
+    {
+        private bool pbSet;
+        private int phaseStep = 10;
+        private int numEntriesSaved = 0;
+        private int numEntriesToAdd = 0;
+        private int numEntriesAdded = 0;
+        private int numFilesToExtract = 0;
+        private int epCycles;
+        long maxBytesXferred = 0;
+
+        public ZipProgress()
+        {
+            return;
+        }
+
+        public ZipProgress(int phaseStep)
+        {
+            this.phaseStep = phaseStep;
+        }
+
+        public void AddProgress(object sender, AddProgressEventArgs e)
+        {
+            switch (e.EventType)
+            {
+                case ZipProgressEventType.Adding_Started:
+                    Console.WriteLine("Adding files to the zip...");
+                    break;
+                case ZipProgressEventType.Adding_AfterAddEntry:
+                    if (!e.CurrentEntry.FileName.EndsWith("/"))
+                    {
+                        numEntriesAdded++;
+                        if (numEntriesAdded % phaseStep == 0)
+                            Console.WriteLine($"Adding file {numEntriesAdded} | {e.CurrentEntry.FileName}");
+                    }
+                    break;
+                case ZipProgressEventType.Adding_Completed:
+                    Console.WriteLine("Added all files" );
+                    break;
+            }
+        }
+
+        public void SaveProgress(object sender, SaveProgressEventArgs e)
+        {
+            switch (e.EventType)
+            {
+                case ZipProgressEventType.Saving_Started:
+                    numEntriesSaved = 0;
+                    Console.WriteLine("Compressing started...");
+                    pbSet = false;
+                    break;
+
+                case ZipProgressEventType.Saving_BeforeWriteEntry:
+                    numEntriesSaved++;
+                    if (numEntriesSaved % phaseStep == 0)
+                        Console.WriteLine($"Compressing {e.CurrentEntry.FileName}");
+                    if (!pbSet)
+                    {
+                        Console.WriteLine($"Total files to compress {e.EntriesTotal}");
+                        pbSet = true;
+                    }
+                    break;
+
+                case ZipProgressEventType.Saving_Completed:
+                    Console.WriteLine("Compressing all files completed.");
+                    pbSet = false;
+                    break;
+
+                //case ZipProgressEventType.Saving_AfterWriteEntry:
+                //    Console.WriteLine("Compressed one file per step");
+                //    break;
+
+                //case ZipProgressEventType.Saving_EntryBytesRead:
+                //    //must be true == (e.BytesTransferred <= e.TotalBytesToTransfer);
+                //    break;
+            }
+        }
+
+        public void ExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            switch (e.EventType)
+            {
+                case ZipProgressEventType.Extracting_BeforeExtractEntry:
+                    if (!pbSet)
+                    {
+                        Console.WriteLine($"pb 1 max {numFilesToExtract}");
+                        pbSet = true;
+                    }
+                    epCycles = 0;
+                    //_pb2Set = false;
+                    break;
+
+                case ZipProgressEventType.Extracting_EntryBytesWritten:
+                    epCycles++;
+                    //Progress for each File extraction, not needed?
+                    //if ((_epCycles % phaseStep) == 0)
+                    //{
+                    //    if (!_pb2Set)
+                    //    {
+                    //        _txrx.Send(String.Format("pb 2 max {0}", e.TotalBytesToTransfer));
+                    //        _pb2Set = true;
+                    //    }
+                    //    _txrx.Send(String.Format("status Extracting {0} :: [{1}/{2}mb] ({3:N0}%)",
+                    //                             e.CurrentEntry.FileName,
+                    //                             e.BytesTransferred / (1024 * 1024),
+                    //                             e.TotalBytesToTransfer / (1024 * 1024),
+                    //                             ((double)e.BytesTransferred / (0.01 * e.TotalBytesToTransfer))
+                    //                             ));
+                    //    string msg = String.Format("pb 2 value {0}", e.BytesTransferred);
+                    //    _txrx.Send(msg);
+                    //}
+                    //if (maxBytesXferred < e.BytesTransferred)
+                    //    maxBytesXferred = e.BytesTransferred;
+                    break;
+
+                case ZipProgressEventType.Extracting_AfterExtractEntry:
+                    Console.WriteLine("pb 1 step");
+                    break;
+            }
+        }
+    }
+}

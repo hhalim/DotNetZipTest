b1a792f8dd821738b06dac6497d7acb12e5b1b23
Hartono Halim <ah3555@Halff.com>
2019-02-01 09:15:50 -0600

    Add project files.

---
+++ DotNetZipTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetZipTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
